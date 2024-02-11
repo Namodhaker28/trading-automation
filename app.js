6055e39a7d48ad5aaa99f3bb7a4482a72b321149
namodhaker28 <namodhaker76@gmail.com>
2024-02-11 10:17:49 +0530

    hft testing

--- app.js
+++ app.js
@@ -14,28 +14,6 @@ const token = "6722173872:AAETsn23sYExosPaUyiB-y4RG-vgGuTtXzw";
 const bot = new TelegramBot(token, { polling: true });
 const chatId = 897911354;
 
-// bot.onText(/\/echo (.+)/, (msg, match) => {
-//   // 'msg' is the received Message from Telegram
-//   // 'match' is the result of executing the regexp above on the text content
-//   // of the message
-
-//   const chatId = msg.chat.id;
-//   const resp = match[1]; // the captured "whatever"
-
-//   // send back the matched "whatever" to the chat
-//   bot.sendMessage(chatId, resp);
-// });
-
-// // Listen for any kind of message. There are different kinds of
-// // messages.
-// bot.on("message", (msg) => {
-//   const chatId = msg.chat.id;
-//   console.log(chatId);
-//   // send a message to the chat acknowledging receipt of their message
-//   bot.sendMessage(chatId, "Received your message");
-// });
-
-
 const binance = new Binance().options({
   APIKEY: "3bVcGQnY5ojwlLA8RMkBeXlOjrMnJZ3ty7mT8HcjPHSnn0KVNsgGEmAVolT5mHNE",
   APISECRET: "4NmUo9uPGIgmurx58pbiHhDY9cr13whM3EWrD2ga9Rg07uB3ZTw3RYF7gPHp46rV",
@@ -46,6 +24,9 @@ const btcAmount = 0.005;
 let quantity;
 
 const automation = async () => {
+    bot.sendMessage(chatId,"Server running")
+  const tradeType = JSON.parse(fs.readFileSync("data.json", "utf-8")).side;
+
   try {
     const sma15 = await getSimpleMovingAverage("BTCUSDT", "15m", 15);
     const sma35 = await getSimpleMovingAverage("BTCUSDT", "15m", 35);
@@ -55,17 +36,18 @@ const automation = async () => {
     // console.log(openPosition)
 
     if (sma15 > sma35) {
-      //   if (JSON.parse(fs.readFileSync("data.json", "utf-8")).side === "short") {
-      if (Number(openPosition[0].positionAmt)) {
+      if (tradeType === "long" && Number(openPosition[0].positionAmt)) {
+        console.log("alredy long position is open");
+        return;
+      }
+      if (tradeType === "short" && Number(openPosition[0].positionAmt)) {
         quantity = Number(openPosition[0].positionAmt) * 2;
-        
       } else {
         quantity = btcAmount;
       }
 
       const broughtOrder = await binance.futuresMarketBuy("BTCUSDT", quantity, { newOrderRespType: "RESULT" });
       console.log("broughtOrder", broughtOrder);
-
 
       const price = Number(broughtOrder.avgPrice) + profitTarget;
       console.log("price ====> ", price);
@@ -75,12 +57,12 @@ const automation = async () => {
 
       bot.sendMessage(chatId, `Trade Placed: ${broughtOrder.avgPrice}  ( ${broughtOrder.origQty} )  \n TP : ${tpOrder.stopPrice} `);
       fs.writeFileSync("data.json", JSON.stringify({ side: "long" }, null, 2));
-      //   } else {
-      //     console.log("alredy short position is  open");
-      //   }
     } else {
-      //   if (JSON.parse(fs.readFileSync("data.json", "utf-8")).side === "long") {
-      if (Number(openPosition[0].positionAmt)) {
+      if (tradeType === "short" && Number(openPosition[0].positionAmt)) {
+        console.log("alredy short position is open");
+        return;
+      }
+      if (tradeType === "long" && Number(openPosition[0].positionAmt)) {
         quantity = Number(openPosition[0].positionAmt) * 2;
       } else {
         quantity = btcAmount;
@@ -97,9 +79,6 @@ const automation = async () => {
 
       bot.sendMessage(chatId, `Trade Placed: ${shortOrder.avgPrice} ( ${shortOrder.origQty} )  \n TP : ${tpOrder.stopPrice} `);
       fs.writeFileSync("data.json", JSON.stringify({ side: "short" }, null, 2));
-      //   } else {
-      //     console.log("alredy short position is  open");
-      //   }
     }
   } catch (error) {
     console.log("error: ", error);
